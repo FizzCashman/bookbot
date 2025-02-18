b5c45d7c5c6ff1104b6c71f14f9f6ace0e35d592
FizzCashman <git@lewismacleod.net>
2025-02-18 13:23:31 +0000

    1.1: Added user path input with validation and exception handling

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+import os
+
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
+    while True:
+        path = get_file_path()
+        try:
+            with open(path) as f:
+                file_contents = f.read()
+            break
+        except IOError:
+            print(f"Unable to read {path}\nPlease try another file")
     words_in_book = word_count(file_contents)
     characters = char_count(file_contents)
-    report(characters, words_in_book)
-    return file_contents
+    report(characters, words_in_book, path)
+
+def get_file_path():
+    while True:
+        path = input("Please input .txt file path: ")
+        if not path.endswith ('.txt'):
+            print("File must be a .txt file!")
+            continue
+        if os.path.exists(path):
+            return path
+        else:
+            print("File not found!")
 
 def word_count(file_contents):
     words = file_contents.split()
@@ -21,7 +39,7 @@ def char_count(file_contents):
             char_dict[i] += 1
     return char_dict
 
-def report(char_dict, word_count):
+def report(char_dict, word_count, path):
     alpha_dict = {}
     char_list = []
     for i in char_dict:
@@ -30,11 +48,11 @@ def report(char_dict, word_count):
     for char, count in alpha_dict.items():
         char_list.append({"char": char, "count": count})
     char_list.sort(reverse=True, key=sort_on)
-    print(f"--- Begin report of books/frankenstein.txt ---\n{word_count} words found in the document")
+    print(f"--- Begin report of {path} ---\n{word_count} words found in the document")
     for i in char_list:
         print(f"The '{i['char']}' character was found {i['count']} times")
     print("--- End report---")
-    
+
 def sort_on(dict):
     return dict["count"]
 
